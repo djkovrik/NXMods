5a311bb86094aa17331055cce029e96389cb9134
Sergey V <djkovrik@gmail.com>
2022-10-29 18:39:06 +0300

    Define api key manager

---
+++ common/domain/src/commonMain/kotlin/com/sedsoftware/nxmods/domain/ApiKeyManager.kt
@@ -0,0 +1,38 @@
+package com.sedsoftware.nxmods.domain
+
+import com.badoo.reaktive.completable.Completable
+import com.badoo.reaktive.completable.completableFromFunction
+import com.badoo.reaktive.completable.subscribeOn
+import com.badoo.reaktive.scheduler.Scheduler
+import com.badoo.reaktive.scheduler.ioScheduler
+import com.badoo.reaktive.single.doOnAfterError
+import com.badoo.reaktive.single.flatMapCompletable
+import com.sedsoftware.nxmods.domain.tools.NxModsApi
+import com.sedsoftware.nxmods.domain.tools.NxModsSettings
+
+class ApiKeyManage(
+    private val api: NxModsApi,
+    private val settings: NxModsSettings,
+    private val scheduler: Scheduler = ioScheduler
+) {
+
+    fun storeApiKey(key: String): Completable =
+        completableFromFunction { settings.apiKey = key }
+            .subscribeOn(scheduler)
+
+    fun validateApiKey(key: String): Completable =
+        api.validateApiKey()
+            .doOnAfterError { settings.isProfileValid = false }
+            .flatMapCompletable { profile ->
+                completableFromFunction {
+                    with(settings) {
+                        name = profile.name
+                        avatar = profile.avatar
+                        isPremium = profile.isPremium
+                        isSupporter = profile.isSupporter
+                        isProfileValid = true
+                    }
+                }
+            }
+            .subscribeOn(scheduler)
+}
--- common/domain/src/commonMain/kotlin/com/sedsoftware/nxmods/domain/tools/NxModsSettings.kt
+++ common/domain/src/commonMain/kotlin/com/sedsoftware/nxmods/domain/tools/NxModsSettings.kt
@@ -1,5 +1,10 @@
 package com.sedsoftware.nxmods.domain.tools
 
 interface NxModsSettings {
+    var name: String
+    var avatar: String
+    var isPremium: Boolean
+    var isSupporter: Boolean
     var apiKey: String
+    var isProfileValid: Boolean
 }
