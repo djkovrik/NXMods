a46d74b719c4d54e7f5a7b75e59d059366c2472a
Sergey V <djkovrik@gmail.com>
2022-10-29 18:44:01 +0300

    Define tracked manager

---
+++ common/domain/src/commonMain/kotlin/com/sedsoftware/nxmods/domain/TrackedManager.kt
@@ -0,0 +1,29 @@
+package com.sedsoftware.nxmods.domain
+
+import com.badoo.reaktive.completable.Completable
+import com.badoo.reaktive.completable.subscribeOn
+import com.badoo.reaktive.scheduler.Scheduler
+import com.badoo.reaktive.scheduler.ioScheduler
+import com.badoo.reaktive.single.flatMapCompletable
+import com.sedsoftware.nxmods.domain.tools.NxModsApi
+import com.sedsoftware.nxmods.domain.tools.NxModsDatabase
+
+class TrackedManager(
+    private val api: NxModsApi,
+    private val db: NxModsDatabase,
+    private val scheduler: Scheduler = ioScheduler
+) {
+
+    fun fetchAllTracked(): Completable =
+        api.getTracked()
+            .flatMapCompletable(db::saveTracked)
+            .subscribeOn(scheduler)
+
+    fun track(domain: String, id: Long): Completable =
+        api.track(domain, id)
+            .subscribeOn(scheduler)
+
+    fun untrack(domain: String, id: Long): Completable =
+        api.untrack(domain, id)
+            .subscribeOn(scheduler)
+}
