f06c9831f388f06c116bde169661f38c3abf61aa
Sergey V <djkovrik@gmail.com>
2022-12-30 19:15:22 +0300

    Auth store tests

--- common/component/auth/src/commonMain/kotlin/com/sedsoftware/nxmods/component/auth/domain/NxModsAuthManager.kt
+++ common/component/auth/src/commonMain/kotlin/com/sedsoftware/nxmods/component/auth/domain/NxModsAuthManager.kt
@@ -1,15 +1,16 @@
 package com.sedsoftware.nxmods.component.auth.domain
 
-import com.badoo.reaktive.completable.Completable
-import com.badoo.reaktive.completable.completableFromFunction
-import com.badoo.reaktive.completable.subscribeOn
+import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.doOnAfterError
-import com.badoo.reaktive.observable.flatMapCompletable
+import com.badoo.reaktive.observable.doOnAfterNext
+import com.badoo.reaktive.observable.map
+import com.badoo.reaktive.observable.subscribeOn
 import com.badoo.reaktive.scheduler.Scheduler
 import com.badoo.reaktive.scheduler.ioScheduler
 import com.badoo.reaktive.single.Single
 import com.badoo.reaktive.single.singleOf
 import com.badoo.reaktive.single.subscribeOn
+import com.sedsoftware.nxmods.component.auth.model.ApiKeyStatus
 import com.sedsoftware.nxmods.domain.tools.NxModsSettings
 
 internal class NxModsAuthManager(
@@ -22,20 +23,19 @@ internal class NxModsAuthManager(
         singleOf(settings.apiKey)
             .subscribeOn(scheduler)
 
-    fun validateApiKey(key: String): Completable =
+    fun validateApiKey(key: String): Observable<ApiKeyStatus> =
         api.validateApiKey(key)
             .doOnAfterError { settings.isProfileValidated = false }
-            .flatMapCompletable { profile ->
-                completableFromFunction {
-                    with(settings) {
-                        name = profile.name
-                        avatar = profile.avatar
-                        isPremium = profile.isPremium
-                        isSupporter = profile.isSupporter
-                        isProfileValidated = true
-                        apiKey = profile.key
-                    }
+            .doOnAfterNext { profile ->
+                with(settings) {
+                    name = profile.name
+                    avatar = profile.avatar
+                    isPremium = profile.isPremium
+                    isSupporter = profile.isSupporter
+                    isProfileValidated = true
+                    apiKey = profile.key
                 }
             }
+            .map { ApiKeyStatus.VALID }
             .subscribeOn(scheduler)
 }
