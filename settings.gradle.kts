3f8e3fed8759ebdc29a7cbf5805045da748d0aa6
DJ_Kovrik <djkovrik@gmail.com>
2023-07-16 18:59:00 +0300

    Update readme

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,10 @@ include(":desktopApp")
 pluginManagement {
     repositories {
         google()
-        gradlePluginPortal()
         mavenCentral()
+        gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://plugins.gradle.org/m2/")
     }
 }
 
